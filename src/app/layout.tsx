6ea417006c6839d477b3a7bf8ff5cea444f9fa55
lloydllim <franzsiblos34@gmail.com>
2025-03-24 15:32:08 +0800

    remove theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from 'react';
 import type { Metadata } from 'next';
 import './globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
   title: 'Junior Web Developer Portfolio',
@@ -15,23 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en'>
       <head>
         <link
           rel='stylesheet'
           href='https://fonts.googleapis.com/css2?family=Arpey:wght@400;500;600;700&display=swap'
         />
       </head>
-      <body>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='dark'
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
